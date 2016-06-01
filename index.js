59c87730899cb6a61eb127a7a1a6efc377be27b8
brusinow <brusinow@me.com>
2016-06-01 12:12:52 -0700

    server is running

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var express= require('express');
 var app = express();
-var db = require("./models");
-var request = require('request');
+
+
 var bodyParser = require('body-parser');
-var flash = require('connect-flash');
+
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/static'));
-app.use(flash());
+
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/family-tree');
 
+app.get('/', function(req, res) {
+  res.send('Hi!');
+});
+
+app.listen(3000);
