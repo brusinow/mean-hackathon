ddeb219448be87e74801044b61e925d5a050a989
brusinow <brusinow@me.com>
2016-06-01 13:35:15 -0700

    added cheerio and request

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express= require('express');
-var app = express();
-
-
+var request = require('request');
+var cheerio = require('cheerio');
 var bodyParser = require('body-parser');
-
+var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,7 +10,7 @@ app.use(express.static(__dirname + '/static'));
 
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/family-tree');
+mongoose.connect('mongodb://localhost/3000');
 
 app.get('/', function(req, res) {
   res.send('Hi!');
