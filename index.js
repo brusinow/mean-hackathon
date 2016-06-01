3f8d5c3bbf16b0fc3ef4fc41969a0e6618b7d6a2
brusinow <brusinow@me.com>
2016-06-01 14:11:21 -0700

    added initial scrape request with cheerio

--- index.js
+++ index.js
@@ -13,7 +13,31 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/3000');
 
 app.get('/', function(req, res) {
-  res.send('Hi!');
+    url = 'http://www.thestranger.com/events//2016-06-01?picks=true';
+
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+            $('.calendar-post-row').filter(function(){
+            var data = $(this);
+            console.log(data);
+            });
+            // Finally, we'll define the variables we're going to capture
+           //  for (i=0;i<data.length;i++)
+           //   var title, date, category, location, price;
+           //   var json = { title : "", date : "", category : "", location : "", price : ""};
+           // }
+        }
+    })
 });
 
 app.listen(3000);
