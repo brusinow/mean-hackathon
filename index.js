6a280edc54f2b953cb3ff003d38adf39a91267c4
brytree <brycebortree@gmail.com>
2016-06-01 18:14:26 -0700

    index.js changes

--- index.js
+++ index.js
@@ -6,20 +6,13 @@ var mongoose = require('mongoose');
 var Event = require('./models/event');
 var app = express();
 
-// app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/static'));
 
 mongoose.connect('mongodb://localhost/fivethings');
 
-
-
 app.get('/api', function(req, res) {
     url = 'http://www.thestranger.com/events//2016-06-01?picks=true';
-
-    // The structure of our request call
-    // The first parameter is our URL
-    // The callback function takes 3 parameters, an error, response status code and the html
 
     request(url, function(error, response, html){
 
@@ -30,36 +23,38 @@ app.get('/api', function(req, res) {
 
             var $ = cheerio.load(html, {normalizeWhitespace: true});
 
-            var results = [];
-
             //console.log($('.calendar-post'));
 
             $('.calendar-post').map(function(i, value) {
-
               var title = $(value).find('.calendar-post-left .calendar-post-title a').text();
-              // var titleParse = title.replace(/([\\n]+[\s]+)/ig, "TESTING THIS OUT");
               var date = $(value).find('.calendar-post-date').text();
               var location = $(value).find('.calendar-post-neighborhood').text();
               var price = $(value).find('.calendar-post-event-price').text();
               var category = $(value).find('.calendar-category').text();
               var link = $(value).find('.calendar-post-title a').attr("href");
               var image = $(value).find('.calendar-post-image img').attr("src");
-              // console.log(typeof image);
-              var item = {
+
+          
+              var newEvent = Event({
                   "title": title, 
-                  "date": date, 
                   "location": location, 
+                  "date": date, 
                   "price": price, 
                   "category": category, 
                   "link": link,
                   "image": image
-              };
-              results.push(item);
+              });
+                newEvent.save(function(err) {
+                if (err) console.log(err);
+                console.log('Event created!');
+                });
+
             });
-            console.log(results);
+          
+
         }
     })
-    res.send(''); 
+    res.send('');
 });
 
 app.listen(3000);
