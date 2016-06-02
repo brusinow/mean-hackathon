5cba0efd7cc7c617d8a75a7ee7a4e333c6d88645
brytree <brycebortree@gmail.com>
2016-06-01 18:22:49 -0700

    database in and beautiful

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,12 @@
 var eventApp = angular.module('eventApp', []);
 eventApp.controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
-
+  // does our api  call
+  $http({url: '/api'}).then(function success(data){
+    $scope.data = data;
+    console.log(data);
+  }, function error(error){
+    $scope.error = error;
+  })
   
 
 }]);
